758fbb38fb2a4449ace8597a40d679dda7e7029a
edwardzcn-decade <edwardzcn98@gmail.com>
2024-04-20 18:12:05 +0800

    feat: init lua files for neovim

---
+++ init.lua
@@ -0,0 +1,39 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ls', ':Neotree filesystem reveal left<CR>', {})
+
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript", "python", "rust", "html"},
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
+})
